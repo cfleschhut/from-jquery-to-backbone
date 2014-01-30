d499b12370176560fedb323d2d89a4eb5f7262bf
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-01-30 14:57:12 +0100

    separating dom & ajax (2)

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,10 @@
-function addStatus() {
+function addStatus(options) {
   $.ajax({
     url: '/status',
     type: 'POST',
     dataType: 'json',
-    data: { text: $('#new-status textarea').val() },
-    success: function(data) {
-      $('#statuses ul').append('<li>' + data.text + '</li>');
-      $('#new-status textarea').val('')
-    }
+    data: { text: options.text },
+    success: options.success
   });
 }
 
@@ -16,7 +13,13 @@ $(document).ready(function() {
   $('#new-status form').on('submit', function(e) {
     e.preventDefault();
 
-    addStatus();
+    addStatus({
+      text: $('#new-status textarea').val(),
+      success: function(data) {
+        $('#statuses ul').append('<li>' + data.text + '</li>');
+        $('#new-status textarea').val('')
+      }
+    });
   });
 
 });
